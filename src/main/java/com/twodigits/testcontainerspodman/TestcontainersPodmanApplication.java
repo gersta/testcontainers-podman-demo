123cbfd1c58490cdbf9915d9e8ec9c067b2ac06d
gerrit.stapper <gerrit.stapper@amarf2gkql0q5.fritz.box>
2022-02-11 14:27:22 +0100

    Initial commit

---
+++ src/main/java/com/twodigits/testcontainerspodman/TestcontainersPodmanApplication.java
@@ -0,0 +1,13 @@
+package com.twodigits.testcontainerspodman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersPodmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainersPodmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twodigits/testcontainerspodman/TestcontainersPodmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twodigits.testcontainerspodman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersPodmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
