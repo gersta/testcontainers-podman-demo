75fd65b6586ac5fbe8f877db6666baa2145233f8
Gerrit Stapper <Gerrit.Stapper@accenture.com>
2022-03-11 13:42:02 +0100

    Simplify TestcontainersPodmanApplication class
    Remove all the lines that were required to run a non-static method in the main class as now the project contains the spring boot web dependency and its application server, which keeps the application open

--- src/main/java/com/twodigits/testcontainerspodman/TestcontainersPodmanApplication.java
+++ src/main/java/com/twodigits/testcontainerspodman/TestcontainersPodmanApplication.java
@@ -1,25 +1,13 @@
 package com.twodigits.testcontainerspodman;
 
-import com.twodigits.testcontainerspodman.characters.CharacterRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class TestcontainersPodmanApplication {
 
-	@Autowired
-	private CharacterRepository repository;
-
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(TestcontainersPodmanApplication.class, args);
-		TestcontainersPodmanApplication app = context.getBean(TestcontainersPodmanApplication.class);
-		app.run();
-	}
-
-	private void run() {
-		System.out.println(repository.findAll());
+		SpringApplication.run(TestcontainersPodmanApplication.class, args)
 	}
 
 }
